41000bb87c3148ecf2e2851d482bc202913628f1
EthanWang <46627960+EthanWXL@users.noreply.github.com>
2019-09-04 19:06:53 +0800

    使用LinkedHashSet代替HashSet,来保证集合元素的顺序 (#28)
    使用LinkedHashSet代替HashSet,来保证集合元素的顺序 (#28)

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -8,16 +9,15 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
+        Set<Character> charSet = new LinkedHashSet<>();
         for (int i = 0; i < s.length(); i++) {
             charSet.add(s.charAt(i));
         }
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Character ch : charSet) {
-            result += ch;
+            result.append(ch);
         }
-
-        return result;
+        return result.toString();
     }
 }
