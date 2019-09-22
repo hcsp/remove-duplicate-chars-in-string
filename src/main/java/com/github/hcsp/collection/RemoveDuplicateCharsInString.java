de5b88e551c53986a36cc60c3307aea9f917d82a
fcbhank <hankhu4work@163.com>
2019-09-22 21:22:39 +0800

    HashSet 换 LinkedHashSet

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -8,16 +8,22 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
+        // HashSet 背后是 HashTable
+        // 不能保证元素的顺序，所以去重后会按照 hashcode 值排序
+        // 改用 LinkedHashSet
+        // 保证元素添加的顺序：LinkedHashSet
+        // 保证元素自然的顺序：TreeSet
+        LinkedHashSet<Character> charSet = new LinkedHashSet<>();
+
         for (int i = 0; i < s.length(); i++) {
             charSet.add(s.charAt(i));
         }
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Character ch : charSet) {
-            result += ch;
+            result.append(ch);
         }
 
-        return result;
+        return result.toString();
     }
 }
