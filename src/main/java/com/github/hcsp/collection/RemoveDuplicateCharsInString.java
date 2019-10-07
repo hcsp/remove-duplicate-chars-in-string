9c884a4d5e7e7300abfa184c5a9ce45b7997d027
orangec <604150318@qq.com>
2019-10-07 13:11:05 +0800

    修复RemoveDuplicateCharsInString中的bug

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -8,7 +10,6 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
         List<Character> order = new LinkedList<>();
         for (int i = 0; i < s.length(); i++) {
             if (order.indexOf(s.charAt(i)) == -1) {
