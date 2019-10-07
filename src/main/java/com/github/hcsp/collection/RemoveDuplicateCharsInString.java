b3399effae48747272c730107d6f04470dc940b6
zhengbigbig <780357902@qq.com>
2019-10-07 15:02:47 +0800

    LinkedHashSet

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,7 +1,7 @@
 package com.github.hcsp.collection;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -9,12 +9,10 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        List<Character> charSet = new LinkedList<>();
+        Set<Character> charSet = new LinkedHashSet<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (!charSet.contains(ch)) {
-                charSet.add(ch);
-            }
+            charSet.add(ch);
         }
 
         String result = "";
