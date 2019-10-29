bbaec6b6e54e5c0447856fe6cf4d574dce87c4a6
wangyujie1207 <82414199@163.com>
2019-10-29 13:22:37 +0800

    有序列表 (#46)
    有序列表 (#46)

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,23 +1,31 @@
 package com.github.hcsp.collection;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class RemoveDuplicateCharsInString {
+
     // 修改这个方法使得它能够输出正确结果：
     // 例如，输入aabbcc返回abc
     // 输入ccbbaa返回cba
     // 输入apple返回aple
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicateCharsInString("aabbc"));
+        System.out.println(removeDuplicateCharsInString("ccbbaa"));
+        System.out.println(removeDuplicateCharsInString("apple"));
+    }
+
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
+        HashSet<Character> charSet = new LinkedHashSet<>();
         for (int i = 0; i < s.length(); i++) {
             charSet.add(s.charAt(i));
         }
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Character ch : charSet) {
-            result += ch;
+            result.append(ch);
         }
-
-        return result;
+        return result.toString();
     }
 }
