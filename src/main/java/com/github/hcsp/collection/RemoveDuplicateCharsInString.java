0753cc43903db19490961664972da70be7d0446d
jser <872440899@qq.com>
2019-12-10 12:25:37 +0800

    F：修复排列顺序的问题

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -8,7 +9,7 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
+        Set<Character> charSet = new LinkedHashSet<>(); //这里要保证输入和输出的顺序一致，HashSet本身无序
         for (int i = 0; i < s.length(); i++) {
             charSet.add(s.charAt(i));
         }
