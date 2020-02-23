25022eccfd671194631f08707c6eacee85984c7e
bonnie1115 <49628449+bonnie1115@users.noreply.github.com>
2020-02-24 01:41:13 +0800

    bugs (#89)
    bugs (#89)

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -8,16 +9,16 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
+        HashSet<Character> charSet = new LinkedHashSet<>();
         for (int i = 0; i < s.length(); i++) {
             charSet.add(s.charAt(i));
         }
 
-        String result = "";
+        StringBuilder stringBuilder = new StringBuilder();
         for (Character ch : charSet) {
-            result += ch;
+                stringBuilder.append(ch);
         }
 
-        return result;
+        return stringBuilder.toString();
     }
 }
