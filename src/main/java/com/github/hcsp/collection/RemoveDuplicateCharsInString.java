e93a0af57236b48261d8d84f43fe63dd110fb856
zeq96 <76999207+zeq96@users.noreply.github.com>
2021-02-25 14:47:51 +0800

    用LinkedHashSet保证添加顺序 (#158)
    用LinkedHashSet保证添加顺序 (#158)

--- src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
+++ src/main/java/com/github/hcsp/collection/RemoveDuplicateCharsInString.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -8,7 +8,8 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
+        // LinkedHashSetkey保证添加顺序，TreeSet保证自然顺序
+        LinkedHashSet<Character> charSet = new LinkedHashSet<>();
         for (int i = 0; i < s.length(); i++) {
             charSet.add(s.charAt(i));
         }
@@ -19,5 +20,10 @@ public class RemoveDuplicateCharsInString {
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicateCharsInString("aabbcc"));
+        System.out.println(removeDuplicateCharsInString("apple"));
     }
 }
